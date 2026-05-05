38f24eb05fd0ea518eee5aa170a4d4332b96857c
jaketockerman <jtockerm@purdue.edu>
2026-05-04 21:56:41 -0400

    netlify vite plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import netlifyPlugin from "@netlify/vite-plugin-react-router";
 import { reactRouter } from "@react-router/dev/vite";
 import { defineConfig, type UserConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
@@ -50,6 +51,7 @@ export default defineConfig({
   },
   plugins: [
     reactRouter(),
+    netlifyPlugin(),
     tsconfigPaths(),
   ],
   build: {
